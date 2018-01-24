8ee45b512a42134f6696fc1396d458e8f92990ce
jamedow <652972575@qq.com>
2018-01-24 16:41:15 +0800

    静态资源服务

---
+++ laodoufang-static/src/main/java/com/jamedow/laodoufang/LaodoufangStaticApplication.java
@@ -0,0 +1,12 @@
+package com.jamedow.laodoufang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaodoufangStaticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaodoufangStaticApplication.class, args);
+    }
+}
