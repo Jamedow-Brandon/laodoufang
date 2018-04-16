7385ea2885a4b85cda283f49d2af00b1979da023
Jamedow <652972575@qq.com>
2018-04-17 00:19:54 +0800

    huayao mail

--- laodoufang-web/src/main/java/com/jamedow/laodoufang/LaodoufangWebApplication.java
+++ laodoufang-web/src/main/java/com/jamedow/laodoufang/LaodoufangWebApplication.java
@@ -2,16 +2,16 @@ package com.jamedow.laodoufang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+//import org.springframework.web.client.RestTemplate;
 
 @Controller
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient∂
 @EnableWebMvc
 public class LaodoufangWebApplication {
 
@@ -19,10 +19,10 @@ public class LaodoufangWebApplication {
         SpringApplication.run(LaodoufangWebApplication.class, args);
     }
 
-    @Bean
-    @LoadBalanced
-    RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
+//    @Bean
+//    @LoadBalanced
+//    RestTemplate restTemplate() {
+//        return new RestTemplate();
+//    }
 
 }
--- laodoufang-web/src/main/java/com/jamedow/laodoufang/web/huayao/HuaYaoController.java
+++ laodoufang-web/src/main/java/com/jamedow/laodoufang/web/huayao/HuaYaoController.java
@@ -1,14 +1,21 @@
 package com.jamedow.laodoufang.web.huayao;
 
 import com.jamedow.laodoufang.entity.Mail;
+import com.jamedow.laodoufang.entity.Product;
+import com.jamedow.laodoufang.entity.ProductExample;
 import com.jamedow.laodoufang.mail.MailService;
+import com.jamedow.laodoufang.mapper.ProductMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 /**
  * @author Jamedow
@@ -20,19 +27,33 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HuaYaoController {
     Logger logger = LoggerFactory.getLogger(getClass());
 
+    @Autowired
+    private ProductMapper productMapper;
+
     @RequestMapping(value = "/index", method = RequestMethod.GET)
     public String index() {
         return "huayao/index";
     }
 
     @RequestMapping(value = "/detail", method = RequestMethod.GET)
-    public String detail() {
-        return "huayao/detail";
+    public ModelAndView detail(Integer productId) {
+        ModelAndView view = new ModelAndView();
+        view.setViewName("huayao/detail");
+
+        view.addObject("productId", productId);
+        return view;
     }
 
     @RequestMapping(value = "/enquirel", method = RequestMethod.GET)
-    public String enquirel() {
-        return "huayao/enquirel";
+    public ModelAndView enquirel(Integer productId) {
+        ModelAndView view = new ModelAndView();
+        view.setViewName("huayao/enquirel");
+
+        List<Product> products = productMapper.selectByExample(new ProductExample());
+
+        view.addObject("products", products);
+        view.addObject("productId", productId);
+        return view;
     }
 
     @RequestMapping(value = "/mailform", method = RequestMethod.GET)
@@ -42,9 +63,21 @@ public class HuaYaoController {
 
     @RequestMapping(value = "/mailsend", method = RequestMethod.POST)
     @ResponseBody
-    public String send(String receiver, String name, String message) {
+    public String send(Integer productId, Integer demand, String requirementDescription, String companyName,
+                       String district, String phone, String province, String region, String receiver) {
+
+        StringBuilder messageBuilder = new StringBuilder();
+        messageBuilder.append("productId:").append(productId);
+        messageBuilder.append("demand:").append(demand);
+        messageBuilder.append("requirementDescription:").append(requirementDescription);
+        messageBuilder.append("companyName:").append(companyName);
+        messageBuilder.append("district:").append(district);
+        messageBuilder.append("phone:").append(phone);
+        messageBuilder.append("province:").append(province);
+        messageBuilder.append("region:").append(region);
+        messageBuilder.append("receiver:").append(receiver);
         Mail mail = new Mail.Builder("smtp.qq.com", "1472541865@qq.com", "tnnfmpbgsjckbade")
-                .sender("1472541865@qq.com").receiver(receiver).name(name).message(message).build();
+                .sender("1472541865@qq.com").receiver(receiver).name(companyName).message(messageBuilder.toString()).build();
         MailService.sendMail(mail);
         return "success";
     }
